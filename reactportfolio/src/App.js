dbb7ff4b4d2057d58606ee3cad8928ce4ca2acfb
Macy <59153195+Macymo@users.noreply.github.com>
2020-07-31 00:15:45 -0400

    initial commit, components, styles

---
+++ reactportfolio/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+import Project from "./components/Project.js";
+
+function App() {
+  return (
+      <div className="App">
+        <Project />
+      </div>
+  );
+}
+
+export default App;
+
