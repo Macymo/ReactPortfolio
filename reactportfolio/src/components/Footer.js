7a061fad1753c27882e8a179f2eb79c0dbaf8f6e
Macy <59153195+Macymo@users.noreply.github.com>
2020-08-02 22:56:04 -0400

    app, components, pages

---
+++ reactportfolio/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "../styles/footer.css"
+
+function Footer() {
+    return (
+    <footer className="footer">
+        <div class="col-md-12">
+            <p role= "img" id="mm">Made by MM<span role="img" aria-label="jsx-a11y/accessible-emoji">❤️</span>
+            </p>
+            <p id="year">2020
+            </p>
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ reactportfolio/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => (
+  <div>
+    <h1>About Page</h1>
+    <p>I am from Clovis, New Mexico. I have always had a desire to learn new things and see different places.
+        After living in the Marshall Islands for eight months, I've recently moved to Cary, North Carolina.
+        As a Cosmetologist for over 10 years and since moving to Cary, I have started an exciting journey towards a new career path in Web Development. 
+        A career in Web Development intrigues me with the everyday opportunity of growth, mental stimulation and the ability to continue channeling my creativity.
+    </p>
+  </div>
+);
+
+export default About;
