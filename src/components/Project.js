dc4cd167c577151ca7fec8b1dc60e7ac9f292ddb
Macy <59153195+Macymo@users.noreply.github.com>
2020-10-10 22:52:45 -0400

    project css

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,31 +1,24 @@
 import React from "react";
 import "../styles/project.css";
 
-function Project() {
+// import TrackerImg from "./assets/fitnessTracker.png";
+// import MarkdownImg from "./assets/mdGenerator.gif";
+// import NoteImg from "./assets/noteTaker.png";
+// import TeamGenerator from "./assets/teamProfileGenerator.gif";
+// import WeatherDash from "./assets/weatherDashboard.png";
+// import WorkSchdlr from "./assets/workDayScheduler.png";
+
+function Project(props) {
     return (
-        <div>
-        <div className="imgBox">
-            <div className="imgCont">
-            <img className="trackerImg" src="./assets/fitnessTracker.png" alt="fitnessTracking"></img>
+        <div className="container">
+            <div className="row">
+                <div className="imgBox">
+                    <div className="imgCont">
+                    <img className="img-fluid" src="./assets/fitnessTracker.png" alt=""></img>
+                    </div>
+                    <div className="colorElement"></div>
+                </div>
             </div>
-            <div className="colorElement"></div>
-        </div>
-        <div className="imgBox">
-            <div className="imgCont">
-            <img className="markDownImg" src="./assets/mdGenerator.gif" alt="mdGenerator"></img>
-            </div>
-            <div className="colorElement"></div>
-        </div>
-        <div className="imgBox">
-            <div className="imgCont">
-            <img className="noteImg" src="./assets/noteTaker.png" alt="noteTaker"></img>
-            </div>
-            <div className="colorElement"></div>
-        </div>
-        
-            <img className="teamGnrtrImg" src="./assets/teamProfileGenerator.gif" alt="teamGenerator"></img>
-            <img className="teatherDashboardImg" src="./assets/weatherDashboard.png" alt="weatherDashboard"></img>
-            <img className="SchdlrImg" src="./assets/workDayScheduler.png" alt="workDayScheduler"></img>
         </div>
         
     );
