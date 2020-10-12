917e08b505e068fb782de0c22e0cf2c95aaed3a1
Macy <59153195+Macymo@users.noreply.github.com>
2020-10-11 21:25:44 -0400

    added projects/css

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,26 +1,36 @@
 import React from "react";
+import projects from "../projects.json";
 import "../styles/project.css";
 
-// import TrackerImg from "./assets/fitnessTracker.png";
-// import MarkdownImg from "./assets/mdGenerator.gif";
-// import NoteImg from "./assets/noteTaker.png";
-// import TeamGenerator from "./assets/teamProfileGenerator.gif";
-// import WeatherDash from "./assets/weatherDashboard.png";
-// import WorkSchdlr from "./assets/workDayScheduler.png";
 
-function Project(props) {
+
+
+function Project() {
     return (
+        projects.map((project) => { 
+        return (
         <div className="container">
             <div className="row">
                 <div className="imgBox">
                     <div className="imgCont">
-                    <img className="img-fluid" src="./assets/fitnessTracker.png" alt=""></img>
+                    <div className="imgBoxText"></div>
+                        <h2> { project.title }</h2>
+                    <img className="img-fluid" src={ project.image } alt="projects"></img> 
                     </div>
                     <div className="colorElement"></div>
+                    <div className="imgBoxCont">
+                        {/* <div className="imgBoxText"></div>
+                        <h2> { project.title }</h2> */}
+                        <div className="subtext">
+                            <p> { project.subtitle } </p>
+                            <a className="button" href={ project.site } target="_blank" rel="noopener noreferrer">{ project.code }</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        
+        )
+    })
     );
 }
 
