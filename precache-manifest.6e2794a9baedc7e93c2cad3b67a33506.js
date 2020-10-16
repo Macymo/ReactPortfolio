075be7b48f9f2f5d1edd3814b88408b05025dbed
Macy <59153195+Macymo@users.noreply.github.com>
2020-10-16 01:42:34 -0400

    Updates

---
+++ precache-manifest.6e2794a9baedc7e93c2cad3b67a33506.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd1e1b94c27294961afd455b5ea9fd17",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "9031a0eee254cce4e037",
+    "url": "/ReactPortfolio/static/css/main.34bcd4be.chunk.css"
+  },
+  {
+    "revision": "7c7b17b8ff2d052ac1dc",
+    "url": "/ReactPortfolio/static/js/2.232333a4.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/ReactPortfolio/static/js/2.232333a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9031a0eee254cce4e037",
+    "url": "/ReactPortfolio/static/js/main.9cd9b6e7.chunk.js"
+  },
+  {
+    "revision": "0114f5eceb8464d5d956",
+    "url": "/ReactPortfolio/static/js/runtime-main.5b3ce1ae.js"
+  }
+]);
