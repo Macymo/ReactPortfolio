a880f2c1ac7a6c1b8cf2dc2ff266acac24faf0eb
Macy <59153195+Macymo@users.noreply.github.com>
2020-10-18 12:47:55 -0400

    deleted projects.json/added projects array to Project.js to render images

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,58 @@
 import React from "react";
-import projects from "../assets/projects.json";
 import "../styles/project.css";
+import schdlr from "../assets/workDayScheduler.png";
+import notes from "../assets/noteTaker.png";
+import weather from "../assets/weatherDashboard.png";
+import fitness from "../assets/fitnessTracker.png";
+import team from "../assets/teamProfileGenerator.gif";
+import mdgenerator from "../assets/mdGenerator.gif";
+
+
+
+let projects = [
+    {
+        title:"Scheduler",
+        subtitle:"Schedule Your Day!",
+        image:schdlr,
+        site:"https://macymo.github.io/WorkDayScheduler/",
+        code:"Live on GitHub Pages"
+    },
+    {
+        title:"Notes",
+        subtitle:"Make Some Notes!",
+        image:notes,
+        site:"https://macysnotetaker.herokuapp.com/",
+        code:"Live on Heroku"
+    },
+    {   
+        title:"Weather",
+        subtitle:"Check the Weather!",
+        image:weather,
+        site:"https://macymo.github.io/WeatherDashboard/",
+        code:"Live on GitHub Pages"
+    },
+    {
+        title:"Fitness",
+        subtitle:"Track Your Fitness!",
+        image:fitness,
+        site:"https://trackingfitness.herokuapp.com",
+        code:"Live on Heroku"
+    },
+    {
+        title:"Team Profiles",
+        subtitle:"Generator Your Team Profiles!",
+        image:team,
+        site:"https://github.com/Macymo/TeamProfileGenerator",
+        code:"GitHub Code"
+    },
+    {
+        title:"ReadMe",
+        subtitle:"MardDown Generator",
+        image:mdgenerator,
+        site:"https://github.com/Macymo/READMEGenerator",
+        code:"GitHub Code"
+    }
+]
 
 function Project() {
     return (
@@ -12,7 +64,7 @@ function Project() {
                     <div className="imgBoxText">
                         <h2> { project.title }</h2>
                         </div>
-                    <img className="img-fluid" src="src/assets/mm.jpg" alt="projects"></img> 
+                    <img className="img-fluid" src={ project.image } alt="projects"></img> 
                     </div>
                     <div className="colorElement"></div>
                     <div className="imgBoxCont">
