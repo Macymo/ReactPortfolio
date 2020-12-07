f48eff1f36c5d6586336538914edd63dcb7772db
Macy <59153195+Macymo@users.noreply.github.com>
2020-12-06 20:16:31 -0500

    Updated CSS

--- build/precache-manifest.7d9bf942712e8128ada4e54aca9a5f9a.js
+++ build/precache-manifest.7d9bf942712e8128ada4e54aca9a5f9a.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "94293fccf5402a67107a67eb90b58fdf",
+    "revision": "2439e30bd1ae59edad4436bccf139974",
     "url": "/ReactPortfolio/index.html"
   },
   {
-    "revision": "9ec3d1f6576fb21842a3",
-    "url": "/ReactPortfolio/static/css/main.56f07b89.chunk.css"
+    "revision": "f3fa101e05a1e9ced0eb",
+    "url": "/ReactPortfolio/static/css/main.16dd3fcf.chunk.css"
   },
   {
     "revision": "2e8e773f1e71bfb7b3e7",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ReactPortfolio/static/js/2.d7f67019.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9ec3d1f6576fb21842a3",
+    "revision": "f3fa101e05a1e9ced0eb",
     "url": "/ReactPortfolio/static/js/main.b6321678.chunk.js"
   },
   {
